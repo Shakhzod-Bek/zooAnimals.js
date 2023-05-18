db901ad0060b8e73e39c7b34e2f97d3205c78ee3
Shakk77777 <shakk7777@gmail.com>
2023-05-17 21:26:26 -0500

    first commit

---
+++ zooAnimals.js
@@ -0,0 +1,21 @@
+/* Hi!
+This is my 
+first javascript project
+Enjoy it!
+ */
+var zooLion
+zooLion = "Alex"
+var zooZebra
+zooZebra = "Marty"
+console.log(zooLion)
+console.log(zooZebra)
+console.log("A lion's name is " + zooLion)
+console.log("A zebra's name is " + zooZebra)
+var lionsound
+lionsound = "ROAAARRR"
+var zebrasound
+zebrasound = "HEHEHEHE"
+console.log(zooLion + " says " + lionsound)
+console.log(zooZebra + " says " + zebrasound)
+lionsound = "MEAOOWW"
+console.log(zooZebra + " now says " + lionsound)
